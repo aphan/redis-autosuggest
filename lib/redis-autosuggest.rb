68c99983962b86c8e04091ac77f4ba9721e26f6f
Adam Phan <aphansh@gmail.com>
2012-09-24 18:58:40 -0700

    Initial Commit

---
+++ lib/redis-autosuggest.rb
@@ -0,0 +1,7 @@
+require "redis-autosuggest/version"
+
+module Redis
+  module Autosuggest
+    # Your code goes here...
+  end
+end
---
+++ lib/redis-autosuggest/version.rb
@@ -0,0 +1,5 @@
+module Redis
+  module Autosuggest
+    VERSION = "0.0.1"
+  end
+end
