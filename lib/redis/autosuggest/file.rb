0b2bb782a5da40ca8419fa612c08885821501f9a
Adam Phan <aphansh@gmail.com>
2012-09-26 02:20:28 -0700

    Supports loading items from a file to the autosuggest redis db

--- lib/redis/autosuggest/file.rb
+++ lib/redis/autosuggest/file.rb
@@ -2,12 +2,19 @@ class Redis
   module Autosuggest
 
     class << self
-      
-      # Add 
+
+      # Add items to the autosuggest database from a file.
+      # Each line be a string representing the item
       def add_from_file(file)
         add(*(File.open(file, "r").map { |l| l.strip }))
       end
-
+      
+      # Add items and their to the autosuggest database from a file.
+      # Each line be a string representing the item followed by its score
+      # Example:
+      # item1 0.4
+      # item2 2.1
+      # item3 5.2
       def add_with_score_from_file(file)
         add_with_score(*(File.open(file, "r").map { |l| l.split(" ")}.flatten))
       end
