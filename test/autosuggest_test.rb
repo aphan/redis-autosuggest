5c1f23365a7912ad370ef094eb78406b72ddb5bf
Adam Phan <aphansh@gmail.com>
2012-09-25 20:39:37 -0700

    Tests for add and add_with_score

--- test/autosuggest_test.rb
+++ test/autosuggest_test.rb
@@ -24,7 +24,7 @@ class TestAutosuggest < MiniTest::Unit::TestCase
   end
 
   def test_adding_an_item
-    Redis::Autosuggest.add(@str1, 5)
+    Redis::Autosuggest.add(@str1)
     assert @db.hgetall(Redis::Autosuggest::Config.items)["0"] == @str1.downcase
     assert @subs.keys.size == @str1.size 
     assert_equal ["0"], @subs.zrevrange("t", 0, -1)
@@ -38,14 +38,28 @@ class TestAutosuggest < MiniTest::Unit::TestCase
     assert_equal ["0"], @subs.zrevrange("test stri", 0, -1)
     assert_equal ["0"], @subs.zrevrange("test strin", 0, -1)
     assert_equal ["0"], @subs.zrevrange("test string", 0, -1)
-    @subs.keys.each { |k| assert @subs.zscore(k, 0) == 5 }
   end
 
   def test_adding_duplicate_item
-    Redis::Autosuggest.add(@str1, 5)
-    Redis::Autosuggest.add(@str1, 5)
-    assert @subs.keys.size == @str1.size
+    Redis::Autosuggest.add(@str1)
+    Redis::Autosuggest.add(@str1)
     assert @db.hgetall(Redis::Autosuggest::Config.items).size == 1
+    assert @subs.keys.size == @str1.size
+  end
+
+  def test_adding_multiple_items
+    Redis::Autosuggest.add("one", "two", "three")
+    assert @db.hgetall(Redis::Autosuggest::Config.items).size == 3
+    assert @subs.keys.size == 10
+  end
+
+    def test_adding_multiple_items_with_scores
+    Redis::Autosuggest.add_with_score("one", 1, "two", 2, "three", 3)
+    assert @db.hgetall(Redis::Autosuggest::Config.items).size == 3
+    assert @subs.keys.size == 10
+    assert_equal 1, @subs.zscore("one", 0)
+    assert_equal 2, @subs.zscore("two", 1)
+    assert_equal 3, @subs.zscore("three", 2)
   end
 
   def test_removing_an_item
@@ -64,7 +78,7 @@ class TestAutosuggest < MiniTest::Unit::TestCase
   end
 
   def test_incrementing_an_items_score
-    Redis::Autosuggest.add(@str1, 5)
+    Redis::Autosuggest.add_with_score(@str1, 5)
     Redis::Autosuggest.increment(@str1)
     @subs.keys.each { |k| assert @subs.zscore(k, 0) == 6 }
     Redis::Autosuggest.increment(@str1, 8)
@@ -74,8 +88,8 @@ class TestAutosuggest < MiniTest::Unit::TestCase
   end
 
   def test_suggesting_items
-    Redis::Autosuggest.add(@str1, 5)
-    Redis::Autosuggest.add("#{@str1} longer", 2)
+    Redis::Autosuggest.add_with_score(@str1, 5)
+    Redis::Autosuggest.add_with_score("#{@str1} longer", 2)
     suggestions =  Redis::Autosuggest.suggest(@str1[0..4])
     assert_equal [@str1.downcase, "#{@str1} longer".downcase], suggestions
   end
@@ -87,9 +101,9 @@ class TestAutosuggest < MiniTest::Unit::TestCase
 
   def test_leaderboard_items
     Redis::Autosuggest::Config.use_leaderboard = true
-    Redis::Autosuggest.add(@str1, 3)
-    Redis::Autosuggest.add("Another item", 5)
-    Redis::Autosuggest.add("Third item", 1)
+    Redis::Autosuggest.add_with_score(@str1, 3)
+    Redis::Autosuggest.add_with_score("Another item", 5)
+    Redis::Autosuggest.add_with_score("Third item", 1)
     top_items = Redis::Autosuggest.leaderboard
     assert_equal ["another item", @str1.downcase, "third item"], top_items 
     Redis::Autosuggest::Config.use_leaderboard = false 
