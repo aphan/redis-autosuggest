07754f09dfdd054c6744b3dc32975d7ba446a6bb
Chris Ostrowski <chris.ostrowski@earlyaprilmedia.com>
2013-04-02 22:58:44 -0500

    Put back eager load.

--- lib/redis/autosuggest/rails/sources.rb
+++ lib/redis/autosuggest/rails/sources.rb
@@ -35,7 +35,7 @@ class Redis
         class << self
 
           def init_rails_sources
-            #Rails.application.eager_load!
+            Rails.application.eager_load!
             Redis::Autosuggest.db.flushdb
             Redis::Autosuggest.rails_sources.each do |model, attrs|
               attrs.each do |column, options|
