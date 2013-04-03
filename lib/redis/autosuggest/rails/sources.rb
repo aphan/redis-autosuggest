23c2e3b22f41c4368858606941510cd5a0ef3440
Chris Ostrowski <chris.ostrowski@earlyaprilmedia.com>
2013-04-02 23:12:48 -0500

    Minor cleanup

--- lib/redis/autosuggest/rails/sources.rb
+++ lib/redis/autosuggest/rails/sources.rb
@@ -35,7 +35,6 @@ class Redis
         class << self
 
           def init_rails_sources
-            puts Rails
             ::Rails.application.eager_load!
             Redis::Autosuggest.db.flushdb
             Redis::Autosuggest.rails_sources.each do |model, attrs|
